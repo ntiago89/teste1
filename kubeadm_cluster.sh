06d815ec6171642714714f13e1c257c2cdafcd2a
nuno-t-simoes <ntiago89@gmail.com>
2018-11-17 01:14:57 +0000

    Update script
    Various corrections done: required parameters, get IP automatically, etc

--- kubeadm_cluster.sh
+++ kubeadm_cluster.sh
@@ -1,65 +1,90 @@
 #!/bin/bash
 
-# This execution must be done with root user, so execute sudo -s before execute this script 
+# !Important note!: This execution must be done with root user, so execute sudo -s before execute this script
+# Extra note: this is an experimental test/version, so some errors could be found. For example, missing validations. Sorry for that. :)
+# This script is prepared to be executed on a VM instance at Google Cloud Platform. Some changes must be done for other platforms.
+
+# Based in the following links:
+# - https://www.youtube.com/watch?v=6ZRphF0wGAM
+# - https://raw.githubusercontent.com/onap/logging-analytics/master/deploy/rancher/oom_rancher_setup.sh
+# - https://stackoverflow.com/questions/47184213/regex-to-match-ip-addresses-but-ignore-localhost
+
 
 usage() {
 cat <<EOF
-Usage: $0 -i [PARAMs]
+Usage: $0 -r [PARAMs]
 NOTE: This execution must be done with root user, so execute sudo -s before execute this script 
 Example of the execution:
-sudo ./kubeadm_cluster.sh -i 104.209.168.116 -r master
--u                  : Display usage
--i [ip]         	: ip = IP of the machine (required)
--r [role]         	: role = type of role wished: master or node
+sudo ./kubeadm_cluster.sh -r master [-i 104.209.168.116]
+-u                      : Display usage
+-r [role]               : role = type of role wished: master or node (required)
+-i [ip]                 : ip = Public IP of the machine
 EOF
 }
-  
+
 function create_master(){
 	# First update packages
 
 	echo "»» Update packages"
-	apt-get update -y
+	sudo apt-get update -y
 
 	echo "»» Install docker.io"
-	apt-get install -qy docker.io
+	sudo apt-get install -qy docker.io
 
 	echo "»» Update packages, add gpg key"
-	apt-get update && apt-get install -y apt-transport-https && curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+	sudo apt-get update && sudo apt-get install -y apt-transport-https && sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
 	echo "»» deb kubernetes"
 	echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list && sudo apt-get update
 
 	echo "»» Update and install kube*"
-	apt-get update && apt-get install -y kubelet kubeadm kubernetes-cni
+	sudo apt-get update && sudo apt-get install -y kubelet kubeadm kubernetes-cni
 
-	echo "»» Initialize kubeadm"
-	set -x
-	kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=$IP
-	set +x
-
-	echo "»» Kube config"
+	echo "»» Enable docker"
+	systemctl enable docker.service
+	
+	echo "»» Initialize kubeadm
+Wait about a minute please..."
+	kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=$IP > kubeadm_join.txt # `ip a|grep -oP "inet \K[0-9.]*(?=.*[^ ][^o]$)"` 
+	echo "Execute the following command in node terminal, as said in \"Important note\" when you execute it: `tail -2 kubeadm_join.txt | head -1`
+	This command is also saved in kubeadm_join.txt file"
+	
+	kubectl apply --filename https://git.io/weave-kube-1.6
+	
+	echo "»» ****Please execute the following commands as regular user****:"
 	sudo cp /etc/kubernetes/admin.conf $HOME/
 	sudo chown $(id -u):$(id -g) $HOME/admin.conf
 	export KUBECONFIG=$HOME/admin.conf
 	echo "export KUBECONFIG=$HOME/admin.conf" | tee -a ~/.bashrc
+	
+	
 
 }
 
 function create_node(){
 	echo "»» Update packages"
-	apt-get update -y
+	sudo apt-get update -y
 
 	echo "»» Install docker.io"
-	apt-get install -qy docker.io
+	sudo apt-get install -qy docker.io
 
 	echo "»» Update packages, add gpg key"
-	apt-get update && apt-get install -y apt-transport-https && curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+	sudo apt-get update && sudo apt-get install -y apt-transport-https && sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
 	echo "»» deb kubernetes"
 	echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list && sudo apt-get update
 
 	echo "»» Update and install kube*"
-	apt-get update && apt-get install -y kubelet kubeadm kubernetes-cni
+	sudo apt-get update && sudo apt-get install -y kubelet kubeadm kubernetes-cni
+
+	echo "»» Enable docker"
+	systemctl enable docker.service
+	
+	echo "
+#############################################################################
+# Important note: execute \"kubeadm join\" from master role in this terminal  #
+#############################################################################"
+
 }
 
 function create_cluster_role() {
@@ -71,20 +96,20 @@ function create_cluster_role() {
 	fi
 }
 
-IP=
 ROLE=
+IP=
 
-while getopts ":i:u:r" PARAM; do
+while getopts ":u:r:i" PARAM; do
   case $PARAM in
     u)
       usage
       exit 1
       ;;
+	r)
+      ROLE=${OPTARG}
+      ;;
     i)
       IP=${OPTARG}
-      ;;
-    r)
-      ROLE=${OPTARG}
       ;;
     ?)
       usage
@@ -93,9 +118,9 @@ while getopts ":i:u:r" PARAM; do
     esac
 done
 
-if [[ -z $IP ]]; then
+if [[ -z $ROLE ]]; then
   usage
   exit 1
 fi
 
-create_cluster_role $IP $ROLE
+create_cluster_role $ROLE $IP
